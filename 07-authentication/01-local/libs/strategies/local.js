493d46fdfc9fa642e94b526a2787a66e9d4fb00c
E.Shangeraev <e.shangeraev@krastsvetmet.ru>
2022-06-02 12:27:54 +0700

    add solution for 7-module 1-task

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+      User.findOne({email}, async (error, user) => {
+        if (error) return done(error);
+        if (!user) return done(null, false, 'Нет такого пользователя');
+
+        const passwordIsOk = await user.checkPassword(password);
+
+        if (!passwordIsOk) {
+          return done(null, false, 'Неверный пароль');
+        } else if (passwordIsOk) {
+          return done(null, user);
+        };
+        return done(null, false, 'Стратегия подключена, но еще не настроена');
+      });
     },
 );
